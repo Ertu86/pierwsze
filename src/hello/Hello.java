56cedc8dce716be79d31ae8cda80178b224ce44b
kurs <kurs@kurs-Komputer>
2018-03-09 12:00:46 +0100

    dodanie klasy konto
    Signed-off-by: kurs <kurs@kurs-Komputer>

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -5,6 +5,8 @@ public class Hello {
 	public static void main(String[] args) {
 System.out.println("Pierwsza wersja");
 System.out.println("Druga wersja");
+	Osoba ala = new Osoba ( "Ala", "Kowalska", "2002-03-03");
+	System.out.println(ala);
 	}
 
 }
