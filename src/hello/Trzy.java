0cf446369796783b2b37f5fe52ae73bd036f9f72
kurs <kurs@kurs-Komputer>
2018-03-09 11:57:37 +0100

    trzecia wersja

---
+++ src/hello/Trzy.java
@@ -0,0 +1,14 @@
+package hello;
+
+import java.time.LocalTime;
+
+public class Trzy {
+
+	public static void main(String[] args) {
+
+System.out.println("Trzecia klasa godzina");
+System.out.println(LocalTime.now());
+
+	}
+
+}
